dd2f20fdaa5cdcd5655b9103105884caa3ce30e5
Mostafa Ali <49001683+MostafaAlyy@users.noreply.github.com>
2023-12-23 21:44:08 +0200

    Update Graph.cpp

--- Templates/Graph.cpp
+++ Templates/Graph.cpp
@@ -5,10 +5,30 @@ vector<int>g[n];
 multiset<int>g[n];
 map<int,set<int >>g;
 
-// dfs 
+/*
+ * DFS
+ * the time complexity of DFS is O(V + E)
+ * The space complexity of DFS is O(V)
+ */
+
 void dfs(int node ){
     vis[node]=1;
     for (auto &nbr :g[node]) {
         if(!vis[nbr]) dfs(nbr);
     }
 }
+
+//if you want to know if the current node is leaf or not use this way
+void dfs(int node){
+    vis[node]=1;
+    bool leaf=1;
+    for (auto &nbr :g[node]) {
+        if(!vis[nbr]) {
+            dfs(nbr);
+            leaf=0;
+        }
+    }
+    if(leaf)
+        ans++;
+}
+
