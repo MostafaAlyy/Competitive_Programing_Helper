5fec70df84abcf54e0edbfbb252f694adfeff540
Mostafa Ali <49001683+MostafaAlyy@users.noreply.github.com>
2023-12-23 20:55:28 +0200

    Update Graph.cpp

--- Templates/Graph.cpp
+++ Templates/Graph.cpp
@@ -5,4 +5,10 @@ vector<int>g[n];
 multiset<int>g[n];
 map<int,set<int >>g;
 
-
+// dfs 
+void dfs(int node ){
+    vis[node]=1;
+    for (auto &nbr :g[node]) {
+        if(!vis[nbr]) dfs(nbr);
+    }
+}
