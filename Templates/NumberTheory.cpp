76043cd2aada084c4e2e85c6e17e84d49d7532b8
Mostafa Ali <49001683+MostafaAlyy@users.noreply.github.com>
2024-03-05 09:30:59 +0200

    Update NumberTheory.cpp

--- Templates/NumberTheory.cpp
+++ Templates/NumberTheory.cpp
@@ -1,1 +1,12 @@
-
+/*
+ * Fast power
+ * the time complexity is O(log(b))
+ * The space complexity is O(log(b))
+ */
+int fastPow(int a, int b, int m) 
+{
+    if (b == 0) return 1;
+    int res = fastPow(a, b / 2,m);
+    if (b % 2) return ((res * res)%m * a%m)%m;
+    else return (res * res)%m;
+}
