83039860279f641a3ab0e4ab46b0647246e45364
Mostafa Ali <49001683+MostafaAlyy@users.noreply.github.com>
2024-03-15 00:32:12 +0200

    Update Bitwise.cpp

--- Templates/Bitwise.cpp
+++ Templates/Bitwise.cpp
@@ -14,3 +14,10 @@ ll flip_bit(ll n,int i)
 {
     return n^(1<<i);
 }
+
+void clear_bits_in_range(int &n,int i,int j){
+    int a=(~0)<<(j+1);
+    int b=(1<<i) -1;
+    int mask=a|b;
+    n=n&mask;
+}
