fddec9f553dfd90556977393fceefa5f6d0d54f0
Mostafa Ali <49001683+MostafaAlyy@users.noreply.github.com>
2024-05-02 01:36:30 +0300

    Update dsu.cpp

--- Templates/dsu.cpp
+++ Templates/dsu.cpp
@@ -1,25 +1,26 @@
 class DSU{
 public:
     int n;
-    vector<int>par, sz;
-    DSU(int _n) : n(_n){        // used in main to initialize the size of my graph
-//        n=_n+1;
-        par.resize(n);
-        sz.resize(n, 1);
-//        for(int i=0;i<n;i++) par[i]=i;
-        iota(par.begin(), par.end(), 0);
+    vector<int>parent, group_size;
+    DSU(int _n) {        
+        n=_n+1;
+        parent.resize(n);
+        group_size.resize(n, 1);
+        // for(int i=0;i<n;i++) parent[i]=i;
+        iota(parent.begin(), parent.end(), 0);
     }
-    inline int findPar(int u){
-        if(par[u]==u) return u;
-        return par[u]= findPar(par[u]);
+    inline int findParent(int u){
+        if(parent[u]==u) return u;
+        return parent[u]= findParent(parent[u]);
     }
     inline bool join(int u, int v){
-        u= findPar(u);
-        v= findPar(v);
+        u= findParent(u);
+        v= findParent(v);
         if(u==v) return false;
-        if(sz[u]>sz[v]) swap(u, v);             // I'm sure that sz[v] is the greater
-        par[u]=v;
-        sz[v]+=sz[u];
+        if(group_size[u]>group_size[v]) 
+            swap(u, v);             // I'm sure that group_size[v] is the greater
+        parent[u]=v;
+        group_size[v]+=group_size[u];
         return true;
     }
 };
